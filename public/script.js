121bbb4d3546447fa66a9ed94c08fbd792b1bb8f
DarkroomLaboratory <darkroomassociation@gmail.com>
2020-08-17 15:38:10 +0400

    second commit

--- public/script.js
+++ public/script.js
@@ -1,33 +1,57 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const newPeer = new Peer(undefined, {
-    host: '/',
-    port: '3001'
+const myPeer = new Peer(undefined, {
+  host: '/',
+  port: '3001'
 })
-
-const videoInstance = document.createElement('video')
-videoInstance.muted = true
-
+const myVideo = document.createElement('video')
+myVideo.muted = true
+const peers = {}
 navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
+  video: true,
+  audio: true
 }).then(stream => {
-    addVideoStrem(videoInstance, stream)
-})
+  addVideoStream(myVideo, stream)
 
-newPeer.on('open', id => {
-    socket.emit('join-room', ROOM_ID, id)
-})
-
-socket.on('user-connected', userId => {
-    console.log('User connected', userId)
-})
-
-
-function addVideoStrem(video, stream){
-    video.srcObject = stream
-    video.addEventListener('loadedmetadata', () => {
-        video.play
+  myPeer.on('call', call => {
+    call.answer(stream)
+    const video = document.createElement('video')
+    console.log("hey")
+    call.on('stream', userVideoStream => {
+      addVideoStream(video, userVideoStream)
     })
-    videoGrid.append(video)
+  })
+
+  socket.on('user-connected', userId => {
+    connectToNewUser(userId, stream)
+  })
+})
+
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
+  const video = document.createElement('video')
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream)
+  })
+  call.on('close', () => {
+    video.remove()
+  })
+
+  peers[userId] = call
+}
+
+function addVideoStream(video, stream) {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  videoGrid.append(video)
 }
